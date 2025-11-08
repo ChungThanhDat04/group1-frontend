6252625ca0aeb89fe16000662c0537c5e4516ae3
ChungThanhDat04 <dat224210@student.nctu.edu.vn>
2025-11-08 18:29:20 +0700

    Update ForgotPassword.jsx

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -8,7 +8,7 @@ const ForgotPassword = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:8000/api/auth/forgot-password', { email });
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/forgot-password`, { email });
             setMessage(res.data.message);
         } catch (error) {
             setMessage(error.response.data.message);
