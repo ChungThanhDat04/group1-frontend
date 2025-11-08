b1406a7c483939b7af2fa56d14183ea9d7cf19e1
ChungThanhDat04 <dat224210@student.nctu.edu.vn>
2025-11-08 18:27:35 +0700

    Update AdminUserList.jsx

--- src/components/AdminUserList.jsx
+++ src/components/AdminUserList.jsx
@@ -13,7 +13,7 @@ const AdminUserList = () => {
                 headers: { Authorization: `Bearer ${token}` }
             };
 
-            const res = await axios.get('http://localhost:8000/api/users', config);
+            const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/auth/login`, config);
             setUsers(res.data);
             setIsError(false);
         } catch (error) {
