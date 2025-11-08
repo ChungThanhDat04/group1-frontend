566a7999f3740e40df2f1e449c87470b2dfef3bb
ChungThanhDat04 <dat224210@student.nctu.edu.vn>
2025-11-08 18:35:10 +0700

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -12,7 +12,7 @@ const Signup = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:8000/api/auth/signup', { name, email, password });
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/signup`, { name, email, password });
             setMessage(res.data.message); // "Đăng ký thành công"
             setIsError(false);
         } catch (error) {
